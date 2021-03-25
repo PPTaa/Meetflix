f07220cbd1f15ab33af11e9e4dada8eb2f46443b
PPTaa <chobo05@naver.com>
2021-03-25 18:25:41 +0900

    Meetflix 홈 컬렉션뷰 구성

--- Meetflix/HomeViewController.swift
+++ Meetflix/HomeViewController.swift
@@ -12,7 +12,22 @@ import AVFoundation
 class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionViewOne.dataSource = self
+        collectionViewTwo.dataSource = self
+        
+        SearchAPI.search("avengers") { movie in
+            self.movieOne = movie
+        }
+        SearchAPI.search("interstellar") { movie in
+            self.movieTwo = movie
+        }
     }
+    
+    @IBOutlet weak var collectionViewOne: UICollectionView! 
+    @IBOutlet weak var collectionViewTwo: UICollectionView!
+    
+    var movieOne: [Movie] = []
+    var movieTwo: [Movie] = []
     
     @IBAction func moreInfo(_ sender: Any) {
         print("click more btn")
@@ -36,10 +51,57 @@ class HomeViewController: UIViewController {
                 
             }
         }
-        
     }
     @IBAction func information(_ sender: Any) {
         print("click information btn")
+        print(movieOne)
+        print(movieTwo)
     }
+    
+}
+
+extension HomeViewController:UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == collectionViewOne {
+            return movieOne.count
+        }
+        if collectionView == collectionViewTwo {
+            return movieTwo.count
+        }
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "oneCell", for: indexPath)
+        if collectionView == collectionViewOne {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "oneCell", for: indexPath) as? CellOne else { return UICollectionViewCell() }
+            let movie = movieOne[indexPath.item]
+            cell.title.text = movie.title
+            return cell
+        }
+        if collectionView == collectionViewTwo {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "oneCell", for: indexPath) as? CellTwo else { return UICollectionViewCell() }
+            let movie = movieTwo[indexPath.item]
+            cell.title.text = movie.title
+            return cell
+        }
+        return cell
+        
+    }
+    
+    
+}
+extension HomeViewController:UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Click Item : \(indexPath.item)")
+    }
+}
+
+class CellOne: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
+    
+}
+class CellTwo: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
     
 }
